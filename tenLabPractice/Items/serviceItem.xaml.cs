cb87a78679e9340eee1f4655700e7886fae6a1d4
alexeys6969 <alexshashin.2023@gmail.com>
2026-09-10 14:07:54 +0500

    feat: разработан интерфейс страницы отчета, добавлены новые изображения и пользовательские элементы

--- tenLabPractice/Items/serviceItem.xaml.cs
+++ tenLabPractice/Items/serviceItem.xaml.cs
@@ -12,21 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using tenLabPractice.Pages;
 
-namespace tenLabPractice
+namespace tenLabPractice.Items
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для serviceItem.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class serviceItem : UserControl
     {
-        public static MainWindow init { get; set; }
-        public MainWindow()
+        public serviceItem()
         {
             InitializeComponent();
-            init = this;
-            frame.Navigate(new mainReport());
         }
     }
 }
--- tenLabPractice/View/mainReport.xaml.cs
+++ tenLabPractice/View/mainReport.xaml.cs
@@ -12,21 +12,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using tenLabPractice.Pages;
+using tenLabPractice.Items;
 
-namespace tenLabPractice
+namespace tenLabPractice.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для mainReport.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class mainReport : Page
     {
-        public static MainWindow init { get; set; }
-        public MainWindow()
+        public mainReport()
         {
             InitializeComponent();
-            init = this;
-            frame.Navigate(new mainReport());
         }
     }
 }
